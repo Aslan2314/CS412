5b0dce8bc0f173fcc25c49ed3957e0a839b92c98
Aslan2314 <113208972+Aslan2314@users.noreply.github.com>
2023-12-06 11:07:35 -0500

    Add files via upload

--- graphs_max_3_sat.py
+++ graphs_max_3_sat.py
@@ -1,8 +1,10 @@
 import time
 import random
 import numpy as np
+import pandas as pd
 from itertools import product
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 
 def evaluate_clause(clause, assignment):
@@ -141,12 +143,15 @@ def create_graphs_compare():
 
 def create_randomness_graph(num_runs=10):
     random.seed(456348)
+    size = []  # Number of variables and clauses
+    variations = []  # diff_counts
     y1 = []
     sample = np.array([i for i in range(1, 101)])
     for x in sample:
+        size.append(x)
         diff_count = 0
         prev_assignment, _ = max_3sat_approx(generate_random_cnf(x, x), x)
-        for _ in range(num_runs):
+        for n in range(num_runs):
             clauses = generate_random_cnf(x, x)
             assignment, _ = max_3sat_approx(clauses, x)
             for i in range(len(assignment)):
@@ -154,24 +159,68 @@ def create_randomness_graph(num_runs=10):
                     diff_count += 1
             prev_assignment = assignment
         diff_count = diff_count // num_runs
+        variations.append(diff_count)
         y1.append(diff_count)
 
     plt.plot(sample, y1)
 
     # Graph setup
-    plt.title("Randomness of Approximation for Max 3-SAT")
+    plt.title("Randomness of Approximation for Max 3-SAT (Number of runs = {})".format(num_runs))
     plt.xlabel("Number of clauses and variables")
     plt.ylabel("Number of Differences")
     plt.grid()
     plt.legend()
     plt.show()
 
+    data = []
+    for i in range(len(size)):
+        arr = [size[i], variations[i]]
+        data.append(arr)
+    frame = pd.DataFrame(np.array(data), columns=["Number of Variables and Clauses", "Number of Differences"])
+    dataframe_to_pdf(frame, 'tables.pdf')
+
+
+def _draw_as_table(df, pagesize):
+    alternating_colors = [['white'] * len(df.columns), ['lightgray'] * len(df.columns)] * len(df)
+    alternating_colors = alternating_colors[:len(df)]
+    fig, ax = plt.subplots(figsize=pagesize)
+    ax.axis('tight')
+    ax.axis('off')
+    the_table = ax.table(cellText=df.values,
+                         rowLabels=df.index,
+                         colLabels=df.columns,
+                         rowColours=['lightblue'] * len(df),
+                         colColours=['lightblue'] * len(df.columns),
+                         cellColours=alternating_colors,
+                         loc='center')
+    return fig
+
+
+def dataframe_to_pdf(df, filename, numpages=(1, 1), pagesize=(11, 8.5)):
+    with PdfPages(filename) as pdf:
+        nh, nv = numpages
+        rows_per_page = len(df) // nh
+        cols_per_page = len(df.columns) // nv
+        for i in range(0, nh):
+            for j in range(0, nv):
+                page = df.iloc[(i * rows_per_page):min((i + 1) * rows_per_page, len(df)),
+                       (j * cols_per_page):min((j + 1) * cols_per_page, len(df.columns))]
+                fig = _draw_as_table(page, pagesize)
+                if nh > 1 or nv > 1:
+                    # Add a part/page number at bottom-center of page
+                    fig.text(0.5, 0.5 / pagesize[0],
+                             "Part-{}x{}: Page-{}".format(i + 1, j + 1, i * nv + j + 1),
+                             ha='center', fontsize=8)
+                pdf.savefig(fig, bbox_inches='tight')
+
+                plt.close()
+
 
 def main():
-    create_graphs_exact()
-    create_graphs_approx()
-    create_graphs_compare()
-    create_randomness_graph()
+    # create_graphs_exact()
+    # create_graphs_approx()
+    # create_graphs_compare()
+    create_randomness_graph(num_runs=50)
 
 
 if __name__ == "__main__":
